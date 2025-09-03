9ca8a7ed2f037b16c897d91da2f94a5a5b0bda51
ُElshaterAhmed <ahmedelshater2013@gmail.com>
2025-09-04 00:00:09 +0300

    edit on login screen

--- lib/auth/view/screen/login_screen.dart
+++ lib/auth/view/screen/login_screen.dart
@@ -1,26 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:movies/auth/view/widget/switch_language.dart';
 import 'package:movies/shared/view/widget/app_theme.dart';
+import 'package:movies/shared/view/widget/customed_button.dart';
+import 'package:movies/shared/view/widget/text_field.dart';
 
 class LoginScreen extends StatefulWidget {
+  static const String routName = "/login";
+  const LoginScreen({super.key});
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  bool _obscurePassword = true;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppTheme.black,
+      backgroundColor: Colors.black,
       body: SafeArea(
         child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 40),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              // Logo
+              // Logo (your image)
               Center(
                 child: Image.asset(
                   "assets/images/Logo.png",
@@ -30,50 +39,28 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
               const SizedBox(height: 50),
 
-              // Email Field
-              TextField(
-                style: TextStyle(color: AppTheme.white),
-                decoration: InputDecoration(
-                  hintText: "Email",
-                  hintStyle: TextStyle(color: Colors.white70),
-                  prefixIcon: Icon(Icons.email, color: AppTheme.white),
-                  filled: true,
-                  fillColor: AppTheme.grey,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-
-              // Password Field
-              TextField(
-                obscureText: _obscurePassword,
-                style: const TextStyle(color: AppTheme.white),
-                decoration: InputDecoration(
-                  hintText: "Password",
-                  hintStyle: TextStyle(color: Colors.white70),
-                  prefixIcon: Icon(Icons.lock, color: AppTheme.white),
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      _obscurePassword
-                          ? Icons.visibility_off
-                          : Icons.visibility,
-                      color: AppTheme.white,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 19),
+                child: Column(
+                  children: [
+                    // Email TextField
+                    CustomTextField(
+                      keyboardType: TextInputType.emailAddress,
+                      label: "Email",
+                      controller: emailController,
+                      prefixImage: "email",
                     ),
-                    onPressed: () {
-                      setState(() {
-                        _obscurePassword = !_obscurePassword;
-                      });
-                    },
-                  ),
-                  filled: true,
-                  fillColor: AppTheme.grey,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide.none,
-                  ),
+                    const SizedBox(height: 22.5),
+
+                    // Password TextField
+                    CustomTextField(
+                      keyboardType: TextInputType.visiblePassword,
+                      label: "Password",
+                      controller: passwordController,
+                      prefixImage: "password",
+                      isPassword: true,
+                    ),
+                  ],
                 ),
               ),
 
@@ -84,99 +71,62 @@ class _LoginScreenState extends State<LoginScreen> {
                   onPressed: () {},
                   child: Text(
                     "Forget Password ?",
-                    style: TextStyle(color: AppTheme.yellow),
+                    style: Theme.of(
+                      context,
+                    ).textTheme.titleSmall!.copyWith(color: AppTheme.yellow),
                   ),
                 ),
               ),
-              const SizedBox(height: 10),
+              const SizedBox(height: 33),
 
               // Login Button
-              SizedBox(
-                width: double.infinity,
-                height: 50,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: AppTheme.yellow,
-                    foregroundColor: AppTheme.black,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    "Login",
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
+              CustomedButton(text: "Login", onPressed: () {}),
+              const SizedBox(height: 22),
 
               // Create Account
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
-                    "Don't Have Account ? ",
-                    style: TextStyle(color: AppTheme.white),
+                    "Don't Have Account ?  ",
+                    style: Theme.of(
+                      context,
+                    ).textTheme.titleSmall!.copyWith(color: AppTheme.white),
                   ),
                   GestureDetector(
                     onTap: () {},
                     child: Text(
                       "Create One",
-                      style: TextStyle(
-                        color: AppTheme.yellow,
-                        fontWeight: FontWeight.bold,
-                      ),
+                      style: Theme.of(
+                        context,
+                      ).textTheme.titleSmall!.copyWith(color: AppTheme.yellow),
                     ),
                   ),
                 ],
               ),
-              const SizedBox(height: 20),
+              const SizedBox(height: 27),
 
               // OR Divider
               Row(
-                children: [
+                children: const [
+                  Expanded(child: Divider(color: AppTheme.yellow, indent: 100)),
+                  Text("   OR   ", style: TextStyle(color: Colors.yellow)),
                   Expanded(
-                    child: Divider(color: AppTheme.yellow, thickness: 1),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8),
-                    child: Text("OR", style: TextStyle(color: AppTheme.yellow)),
-                  ),
-                  Expanded(
-                    child: Divider(color: AppTheme.yellow, thickness: 1),
+                    child: Divider(color: AppTheme.yellow, endIndent: 100),
                   ),
                 ],
               ),
               const SizedBox(height: 20),
 
-              // Google Login
-              SizedBox(
-                width: double.infinity,
-                height: 50,
-                child: ElevatedButton.icon(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: AppTheme.yellow,
-                    foregroundColor: AppTheme.black,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                  ),
-                  onPressed: () {},
-                  icon: Image.asset(
-                    "assets/images/google.png",
-                    width: 30,
-                    height: 30,
-                  ),
-                  label: Text(
-                    "Login With Google",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                ),
+              // Google Login Button
+              CustomedButton(
+                text: "Login With Google",
+                onPressed: () {},
+                imageName: "google",
               ),
-              const SizedBox(height: 30),
+              const SizedBox(height: 28),
 
-              // Switch Language Widget
+              // Language Buttons
               SwitchLanguage(onTapEnlish: () {}, onTapArabic: () {}),
             ],
           ),
