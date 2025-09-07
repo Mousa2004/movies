022c922b7fecb6b7e403f543acce2570385421e3
Moamen <moamenfares573@gmail.com>
2025-09-07 13:38:35 +0300

    Connected Search screen to bottom navigation

--- lib/home/view/screen/movie_list_screen.dart
+++ lib/home/view/screen/movie_list_screen.dart
@@ -3,26 +3,26 @@ import 'package:movies/shared/data/mock_movies.dart';
 import 'package:movies/shared/view/widget/movie_card.dart';
 
 class MovieListScreen extends StatelessWidget {
-  const MovieListScreen({super.key});
+    const MovieListScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Movies")),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(16),
-        itemCount: kDemoMovies.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,          // عمودين
-          crossAxisSpacing: 16,       // المسافة بين الأعمدة
-          mainAxisSpacing: 16,        // المسافة بين الصفوف
-          childAspectRatio: 0.7,      // نسبة العرض للطول (عشان الكارت يبقى طويل شوية)
-        ),
-        itemBuilder: (context, index) {
-          final movie = kDemoMovies[index];
-          return MovieCard(movie: movie);
-        },
-      ),
-    );
-  }
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(title: const Text("Movies")),
+            body: GridView.builder(
+                padding: const EdgeInsets.all(16),
+                itemCount: kDemoMovies.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,          
+                    crossAxisSpacing: 16,      
+                    mainAxisSpacing: 16,        
+                    childAspectRatio: 0.7
+                ),
+                itemBuilder: (context, index) {
+                    final movie = kDemoMovies[index];
+                    return MovieCard(movie: movie);
+                }
+            )
+        );
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -8,33 +8,33 @@ import 'home/view/screen/update_profile_screen.dart';
 import 'auth/forgetpassword/ForgetPasswordPage.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();    ///في المينasync لازم تتكتب عشان انت استخدمتAli
-  final prefs = await SharedPreferences.getInstance();
-  final bool showOnBoarding = prefs.getBool("onboarding") ?? true;///Ali
+    WidgetsFlutterBinding.ensureInitialized();    ///في المينasync لازم تتكتب عشان انت استخدمتAli
+    final prefs = await SharedPreferences.getInstance();
+    final bool showOnBoarding = prefs.getBool("onboarding") ?? true;///Ali
 
-  runApp(MoviesApp(showOnBoarding: showOnBoarding));
+    runApp(MoviesApp(showOnBoarding: showOnBoarding));
 }
 
 class MoviesApp extends StatelessWidget {
-  MoviesApp({super.key, required this.showOnBoarding});///Ali
-  final bool showOnBoarding;
+    MoviesApp({super.key, required this.showOnBoarding});///Ali
+    final bool showOnBoarding;
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: showOnBoarding ? OnBoarding.routeName : HomeScreen.routName, ///Ali
-      routes: {
-        HomeScreen.routName: (_) => HomeScreen(),
-        RegisterScreen.routName: (_) => RegisterScreen(),
-        OnBoarding.routeName: (_) => OnBoarding(),
-        UpdateProfileScreen.routeName: (_) => UpdateProfileScreen(),
-        ForgetPasswordPage.routName: (_) => ForgetPasswordPage(),
-      },
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.dark,
-    );
-  }
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            initialRoute: showOnBoarding ? OnBoarding.routeName : HomeScreen.routName, ///Ali
+            routes: {
+                HomeScreen.routName:(_) => HomeScreen(),
+                RegisterScreen.routName:(_) => RegisterScreen(),
+                OnBoarding.routeName:(_) => OnBoarding(),
+                UpdateProfileScreen.routeName:(_) => UpdateProfileScreen(),
+                ForgetPasswordPage.routName:(_) => ForgetPasswordPage()
+            },
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            themeMode: ThemeMode.dark
+        );
+    }
 }
 
