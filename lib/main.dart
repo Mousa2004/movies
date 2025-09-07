f3c5fca20a3371aba0b77641bba2b3446b9eeda9
Mohamed Ayman <mousa2692004@gmail.com>
2025-09-07 23:26:54 +0300

    resolve conflic main

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ import 'home/view/screen/update_profile_screen.dart';
 import 'auth/view/screen/ForgetPasswordPage.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = MyBlocObserver();
 
@@ -22,17 +21,11 @@ void main() async {
 
   ///Ali
 
-    WidgetsFlutterBinding.ensureInitialized();    ///في المينasync لازم تتكتب عشان انت استخدمتAli
-    final prefs = await SharedPreferences.getInstance();
-    final bool showOnBoarding = prefs.getBool("onboarding") ?? true;///Ali
-
-
-    runApp(MoviesApp(showOnBoarding: showOnBoarding));
+  runApp(MoviesApp(showOnBoarding: showOnBoarding));
 }
 
 class MoviesApp extends StatelessWidget {
-
-  MoviesApp({super.key, required this.showOnBoarding});
+  const MoviesApp({super.key, required this.showOnBoarding});
 
   ///Ali
   final bool showOnBoarding;
@@ -61,26 +54,4 @@ class MoviesApp extends StatelessWidget {
       ),
     );
   }
-
-    MoviesApp({super.key, required this.showOnBoarding});///Ali
-    final bool showOnBoarding;
-
-    @override
-    Widget build(BuildContext context) {
-        return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            initialRoute: showOnBoarding ? OnBoarding.routeName : HomeScreen.routName, ///Ali
-            routes: {
-                HomeScreen.routName:(_) => HomeScreen(),
-                RegisterScreen.routName:(_) => RegisterScreen(),
-                OnBoarding.routeName:(_) => OnBoarding(),
-                UpdateProfileScreen.routeName:(_) => UpdateProfileScreen(),
-                ForgetPasswordPage.routName:(_) => ForgetPasswordPage()
-            },
-            theme: AppTheme.lightTheme,
-            darkTheme: AppTheme.darkTheme,
-            themeMode: ThemeMode.dark
-        );
-    }
-
 }
