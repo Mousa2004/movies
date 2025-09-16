e018a64a07e8237ebf775c4ab52684c565a56eee
Moamen <moamenfares573@gmail.com>
2025-09-16 15:57:43 +0300

    Add Genres to Movie Details Screen

--- lib/movies/view/screen/movie_detials.dart
+++ lib/movies/view/screen/movie_detials.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:movies/movies/data/models/movie_model.dart';
 import 'package:movies/movies/view/widget/customed_watch.dart';
+import 'package:movies/movies/view/widget/customed_genres.dart';
 
-class MovieDetials extends StatelessWidget {
-  static const String routeName = '/moviedetials';
-  MovieDetials({super.key});
+class MovieDetails extends StatelessWidget {
+    static const String routName = '/moviedetails';
+    final MovieModel movie;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(child: Column(children: [CustomedWatch()])),
-    );
-  }
+    const MovieDetails({super.key, required this.movie});
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            body: SingleChildScrollView(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                        CustomedWatch(movie: movie),
+                        if (movie.genres != null && movie.genres!.isNotEmpty)
+                        Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                            child: SizedBox(
+                                width: double.infinity,
+                                child: CustomedGenres(genres: movie.genres!)
+                            )
+                        )
+                    ]
+                )
+            )
+        );
+    }
 }
